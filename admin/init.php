ca17534cc3686da483e56cf94fba4f01de20a509
Vincent Klaiber <vincentklaiber@gmail.com>
2013-10-22 09:02:56 +0200

    Add a template directory constant.

--- admin/init.php
+++ admin/init.php
@@ -3,13 +3,14 @@
 /**
  * Define Constants.
  */
-define('ADMIN_DIR', get_template_directory() .'/admin');
-define('ADMIN_URL', get_template_directory_uri() .'/admin');
+define('TEMPLATE_DIR', get_template_directory());
+define('ADMIN_DIR', get_template_directory().'/admin');
+define('ADMIN_URL', get_template_directory_uri().'/admin');
 
 define('AUTHOR', 'Vincent Klaiber');
 define('AUTHOR_URL', 'http://vinkla.com');
 
-define('LOGIN_IMAGE_PATH', ADMIN_URL.'/images/admin-login-logo.png');
+define('LOGIN_IMAGE_PATH', TEMPLATE_DIR.'/images/admin-login-logo.png');
 define('LOGIN_HEADER_URL', 'http://vinkla.com');
 
 /**
