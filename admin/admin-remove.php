2e9ef1f80d29ef98a923a44daa41af02357bc7ee
Vincent Klaiber <vincentklaiber@gmail.com>
2013-10-22 16:03:20 +0200

    Remove plugins update messages.

--- admin/admin-remove.php
+++ admin/admin-remove.php
@@ -71,12 +71,20 @@ add_action('wp_dashboard_setup', function()
 });
 
 /**
- * Remove update messages.
+ * Remove core update messages.
  *
  * @return void
  */
 add_action('init', create_function('$a', "remove_action('init', 'wp_version_check');"), 2);
 add_filter('pre_option_update_core', create_function('$a', "return null;"));
+
+/**
+ * Remove plugins update messages.
+ *
+ * @return void
+ */
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter('pre_site_transient_update_plugins', create_function('$a', "return null;"));
 
 /**
  * Remove links from admin toolbar.
