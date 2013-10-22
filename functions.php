72e974f399697086df096fc01772e0362ff77999
Vincent Klaiber <vincentklaiber@gmail.com>
2013-10-22 08:46:39 +0200

    Filter wp_title for better SEO and title.

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@
  */
 $tempdir = get_template_directory();
 require_once($tempdir.'/admin/init.php');
+
+/**
+ * Filter wp_title with name and description.
+ */
+add_filter('wp_title', function($title)
+{
+	$name = get_bloginfo('name');
+	$description = get_bloginfo('description');
+
+	if (is_front_page()) { return "$name - $description"; }
+
+	return trim($title).' - '.$name;
+});
