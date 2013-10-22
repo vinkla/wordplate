da767db63dbc36b147744559c3766e06ce51ba93
Vincent Klaiber <vincentklaiber@gmail.com>
2013-10-22 08:57:27 +0200

    Add theme set up settings.

--- functions.php
+++ functions.php
@@ -7,6 +7,20 @@ $tempdir = get_template_directory();
 require_once($tempdir.'/admin/init.php');
 
 /**
+ * Theme set up settings.
+ */
+add_action('after_setup_theme', function()
+{
+	// Configure WP 2.9+ Thumbnails.
+	add_theme_support('post-thumbnails');
+	set_post_thumbnail_size(50, 50, true);
+	add_image_size('thumbnail-large', 500, '', false);
+
+	// Add support for post formats.
+	add_theme_support('post-formats', ['aside', 'gallery', 'image', 'link', 'quote', 'video', 'audio']);
+});
+
+/**
  * Configure Default Title.
  */
 add_filter('wp_title', function($title)
@@ -25,4 +39,12 @@ add_filter('wp_title', function($title)
 add_filter('wp_trim_excerpt', function($excerpt)
 {
 	return str_replace('[...]', '…', $excerpt);
+});
+
+/**
+ * Change Default Excerpt Length.
+ */
+add_filter('excerpt_length', function($length)
+{
+	return 55;
 });
