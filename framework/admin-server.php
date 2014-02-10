75eef58e50be99ff0a1606a9b35f0e20a46e7120
Vincent Klaiber <vincent@schimpanz.com>
2014-02-10 08:58:58 +0100

    Add PHP and MYSQL information.

--- framework/admin-server.php
+++ framework/admin-server.php
@@ -23,12 +23,14 @@
  */
 add_action('admin_menu', function()
 {
+	global $wpdb;
+
 	$parent = 'options-general.php';
 	$title = 'Server Information';
 	$permission = 'update_core';
 	$slug = 'server-information';
 
-	add_submenu_page($parent, $title, $title, $permission, $slug, function()
+	add_submenu_page($parent, $title, $title, $permission, $slug, function() use ($wpdb)
 	{
 		require TEMPLATE_DIR.'/framework/views/server-information.php';
 	});
--- framework/views/server-information.php
+++ framework/views/server-information.php
@@ -2,8 +2,13 @@
 	<h2>Server Information</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
-	<h3 class="title">Server</h3><!-- /title -->
-	<table class="widefat">
+	<table class="widefat" style="margin:0 0 20px;">
+		<thead>
+			<tr>
+				<th><strong>Server</strong></th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
 		<tbody>
 			<tr>
 				<td>Operating System</td>
@@ -19,7 +24,7 @@
 			</tr>
 			<tr>
 				<td>IP</td>
-				<td><?php echo $_SERVER['SERVER_ADDR']; ?>:<?php echo $_SERVER['SERVER_PORT']; ?></td>
+				<td><?php echo sprintf('%s:%s', $_SERVER['SERVER_ADDR'], $_SERVER['SERVER_PORT']); ?></td>
 			</tr>
 			<tr>
 				<td>Admin</td>
@@ -36,24 +41,71 @@
 		</tbody>
 	</table><!-- /widefat -->
 
-	<h3 class="title">PHP</h3><!-- /title -->
-	<table class="widefat">
+	<table class="widefat" style="margin:0 0 20px;">
+		<thead>
+			<tr>
+				<th><strong>PHP</strong></th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
 		<tbody>
 			<tr>
 				<td>Version</td>
 				<td><?php echo PHP_VERSION; ?></td>
 			</tr>
+			<tr>
+				<td>Memory Limit</td>
+				<td><?php echo ini_get('memory_limit') ? ini_get('memory_limit') : 'N/A'; ?></td>
+			</tr>
+			<tr>
+				<td>Max Post Size</td>
+				<td><?php echo ini_get('post_max_size') ? ini_get('post_max_size') : 'N/A'; ?></td>
+			</tr>
+			<tr>
+				<td>Max Upload Size</td>
+				<td><?php echo ini_get('upload_max_filesize') ? ini_get('upload_max_filesize') : 'N/A'; ?></td>
+			</tr>
+			<tr>
+				<td>Max Execution Time</td>
+				<td><?php echo ini_get('max_execution_time') ? ini_get('max_execution_time') : 'N/A'; ?></td>
+			</tr>
 		</tbody>
 	</table><!-- /widefat -->
 
-	<h3 class="title">Database</h3><!-- /title -->
-	<table class="widefat">
-			</thead>
+	<table class="widefat" style="margin:0 0 20px;">
+		<thead>
+			<tr>
+				<th><strong>MySQL</strong></th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
 		<tbody>
 			<tr>
-				<td>Key</td>
-				<td>Value</td>
+				<td>Version</td>
+				<td><?php echo $wpdb->get_var("SELECT VERSION() AS version"); ?></td>
 			</tr>
+			<tr>
+				<td>Data Usage</td>
+				<td>
+					<?php
+					$data = '';
+					$tables = $wpdb->get_results('SHOW TABLE STATUS');
+					foreach ($tables as $table) { $data += $table->Data_length; }
+					echo $data ? sprintf('%s bytes', $data) : 'N/A';
+					?>
+				</td>
+			</tr>
+			<tr>
+				<td>Max Allowed Connections</td>
+				<td>
+					<?php
+					$query = $wpdb->get_row("SHOW VARIABLES LIKE 'max_connections'");
+					$value = $query->Value;
+					echo $value ? $value : 'N/A';
+					?>
+				</td>
+			</tr>
+
 		</tbody>
 	</table><!-- /widefat -->
 </div><!-- /wrap -->
