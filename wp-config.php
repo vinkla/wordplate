d3ea4e18d2ef7c5531caf1b1438a063884d6c2cb
Vincent Klaiber <vincentklaiber@gmail.com>
2015-03-17 17:40:49 +0100

    Add latest wp-config.php docs.

--- wp-config.php
+++ wp-config.php
@@ -3,9 +3,9 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
@@ -20,8 +20,8 @@ require __DIR__.'/vendor/autoload.php';
 /** Load .env file into WordPress. */
 Dotenv::load(__DIR__);
 
-// ** MySQL settings - You can get this info from your web host. ** //
-/** The name of the database for WordPress. */
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
 define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username. */
