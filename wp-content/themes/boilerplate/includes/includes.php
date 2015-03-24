b0b2074c581efeeb45e806bbd8ded13257335ac9
Vincent Klaiber <vincentklaiber@gmail.com>
2015-03-24 16:34:29 +0100

    Add sidebars and widgets folders.

--- wp-content/themes/boilerplate/includes/includes.php
+++ wp-content/themes/boilerplate/includes/includes.php
@@ -1,11 +1,21 @@
 <?php
 
 /**
- * Register Custom Post Types.
+ * Register custom post types.
  */
 require get_template_directory().'/includes/post-types/book.php';
 
 /**
- * Register Menus.
+ * Register menus.
  */
-require get_template_directory().'/includes/menus/primary-menu.php';
+require get_template_directory().'/includes/menus/primary-enu.php';
+
+/**
+ * Register sidebars.
+ */
+// require get_template_directory().'/includes/sidebars/left-sidebar.php';
+
+/**
+ * Register widgets.
+ */
+// require get_template_directory().'/includes/widgets/facebook.php';
