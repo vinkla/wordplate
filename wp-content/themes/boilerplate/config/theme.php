01becc10da06c97a2a4ce5e295b2fcc11eb4bfea
Vincent Klaiber <vincentklaiber@gmail.com>
2015-03-30 11:20:39 +0200

    Add theme slug to boilerplate.

---
+++ wp-content/themes/boilerplate/config/theme.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Slug
+    |--------------------------------------------------------------------------
+    |
+    | Themes must provide a unique slug for anything in the public namespace,
+    | including translation textdomain, all custom function names, classes,
+    | hooks, public/global variables, database entries (Theme options, post
+    | custom metadata, etc.)
+    |
+    */
+
+    'slug' => 'boilerplate',
+
+];
--- wp-content/themes/boilerplate/library/config.php
+++ wp-content/themes/boilerplate/library/config.php
@@ -7,6 +7,7 @@ return [
     'footer' => require get_template_directory().'/config/footer.php',
     'login' => require get_template_directory().'/config/login.php',
     'menus' => require get_template_directory().'/config/menus.php',
+    'theme' => require get_template_directory().'/config/theme.php',
     'plugins' => require get_template_directory().'/config/plugins.php',
     'widgets' => require get_template_directory().'/config/widgets.php',
 ];
