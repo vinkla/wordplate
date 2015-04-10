e26d8725499ea05d7c9be1ee330c7c1412578bb1
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-10 16:14:22 +0200

    Use laravels ends_with.

--- wp-content/themes/boilerplate/library/modules/theme.php
+++ wp-content/themes/boilerplate/library/modules/theme.php
@@ -9,20 +9,14 @@
  * file that was distributed with this source code.
  */
 
-function endsWith($haystack, $needle) {
-    $length = strlen($needle);
-    if ($length == 0) { return true; }
-    return (substr($haystack, -$length) === $needle);
-}
-
  /**
  * Fix home URL on theme activation
  *
  * @return void
  */
 add_action('after_setup_theme', function () {
-	$url = get_option('home');
-	if (endsWith($url, '/wordpress')) {
-		update_option('home', str_replace('/wordpress', '', $url));
-	}
+    $url = get_option('home');
+    if (ends_with($url, '/wordpress')) {
+        update_option('home', str_replace('/wordpress', '', $url));
+    }
 });
