0bbca84929d5a6c8ac5f860bdf0f95ca9df5ce44
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-19 18:49:47 +0200

    Add default pages.

---
+++ config/pages.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Pages
+    |--------------------------------------------------------------------------
+    |
+    | Setup default pages when your theme is activated. You can also specify
+    | which type of page it is our if the page should use a custom template.
+    |
+    | Accepted parameters: title, type, template and parent.
+    |
+    */
+
+    'pages' => [
+        [
+            'title' => 'Start',
+            'type' => 'home',
+        ],
+        [
+            'title' => 'About',
+            'template' => 'pages/about.php',
+        ],
+        [
+            'title' => 'Archive',
+            'type' => 'blog',
+            'parent' => 'About',
+        ],
+    ]
+];
