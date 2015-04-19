3b4e5d36969e68a3a7692af0934b85efbfddb9e7
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-19 15:12:38 +0200

    Add Google Analytics to configuration
    This closes #68

---
+++ config/analytics.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google Analytics ID
+    |--------------------------------------------------------------------------
+    |
+    | This defines the Google Analytics Tracking ID to use.
+    |
+    | Default to ''.
+    |
+    */
+
+    'google_analytics_id' => env('GOOGLE_ANALYTICS_ID'),
+
+];
