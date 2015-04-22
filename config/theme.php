22efae845a5e9b937bde7e8f32052051fe195554
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-22 22:17:31 +0200

    Remove app.php and add the keys to theme.php
    This closes #79

--- config/theme.php
+++ config/theme.php
@@ -4,6 +4,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
+
+    'debug' => env('WP_DEBUG', false),
+    
+    /*
+    |--------------------------------------------------------------------------
     | Theme Slug
     |--------------------------------------------------------------------------
     |
@@ -15,5 +28,54 @@ return [
     */
 
     'slug' => env('WP_THEME', 'wordplate'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | WordPress Updates
+    |--------------------------------------------------------------------------
+    |
+    | Enable or disable WordPress core, plugins and themes updates.
+    |
+    */
+
+    'updates' => env('WP_ENV', 'production') !== 'production',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    |
+    */
+
+    'timezone' => 'UTC',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | GZIP
+    |--------------------------------------------------------------------------
+    |
+    | Enable Gzip if available. Set this to false if you want to remove GZIP
+    | support from your theme.
+    |
+    */
+
+    'gzip' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disallow File Edit
+    |--------------------------------------------------------------------------
+    |
+    | Occasionally you may wish to disable the plugin or theme editor to
+    | prevent overzealous users from being able to edit sensitive files and
+    | potentially crash the site.
+    |
+    */
+
+    'disallow_file_edit' => true,
 
 ];
