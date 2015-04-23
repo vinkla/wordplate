5e10d9b38a8d83eaa61b7c30002bab1dcaf74d0f
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-23 19:17:08 +0200

    Update header html
    This closes #85

--- public/wp-content/themes/wordplate/header.php
+++ public/wp-content/themes/wordplate/header.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html class="no-js" lang="en">
+<html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta name="viewport" content="width=device-width">
   <meta name="theme-color" content="#464646">
 
   <title><?php wp_title(); ?></title>
 
+  <meta name="author" content="">
   <meta name="description" content="">
 
   <?php wp_head(); ?>
