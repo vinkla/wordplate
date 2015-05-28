0acc5fb60d5684146124fda4821248c14dc93e3a
chris andersson <chris@puredazzle.se>
2015-05-28 12:07:45 +0200

    Added charset and language attributes to header
    Experienced troubles in Firefox 36.0.1 with foreign and special characters.

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width">
   <meta name="theme-color" content="#464646">
