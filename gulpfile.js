6d6061facf77b3ad18af9e74b365e43955b77b5b
Chris Andersson <chris@puredazzle.se>
2015-07-08 13:59:35 +0200

    structure for elixir

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var elixir = require('laravel-elixir');
+
+elixir.config.publicPath = 'public/themes/wordplate/assets';
+elixir.config.css.outputFolder = elixir.config.css.sass.folder = 'styles';
+elixir.config.js.folder = elixir.config.js.outputFolder = 'scripts';
+
+elixir(function(mix) {
+    mix.sass('app.scss')
+    	.browserify('app.js')
+    	.copy(elixir.config.assetsPath + '/images', elixir.config.publicPath + '/images');
+});
