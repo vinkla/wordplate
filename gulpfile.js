bb46ddf32baaa4b402056e1806b72dcb61e9a065
Vincent Klaiber <vincentklaiber@gmail.com>
2015-08-21 10:26:17 +0200

    Add comment about elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var elixir = require('laravel-elixir');
 
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your WordPlate application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
 elixir.config.publicPath = 'public/themes/wordplate/assets';
 elixir.config.css.outputFolder = elixir.config.css.sass.folder = 'styles';
 elixir.config.js.folder = elixir.config.js.outputFolder = 'scripts';
