415d0fac22fe4623c6e1cf14e9eeab61181a9453
Vincent Klaiber <vincentklaiber@gmail.com>
2016-02-22 20:45:36 +0100

    Move assets directory

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,11 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir.config.publicPath = 'public/themes/wordplate/assets';
-elixir.config.css.outputFolder = elixir.config.css.sass.folder = 'styles';
-elixir.config.js.folder = elixir.config.js.outputFolder = 'scripts';
+elixir.config.publicPath = 'public/assets';
+elixir.config.css.outputFolder = 'styles'
+elixir.config.css.sass.folder = 'styles';
+elixir.config.js.folder = 'scripts'
+elixir.config.js.outputFolder = 'scripts';
 
 elixir(function(mix) {
   mix.sass('app.scss')
