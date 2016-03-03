171439190be89850b62b6056a706de42b03a77ff
Vincent Klaiber <vincentklaiber@gmail.com>
2016-03-03 16:52:58 +0100

    Use elixir helper method

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -32,9 +32,9 @@ add_action('after_setup_theme', function () {
 add_action('wp_enqueue_scripts', function () {
     wp_deregister_script('jquery');
 
-    // wp_enqueue_style('wordplate', get_template_directory_uri().'/assets/styles/wordplate.css');
+    // wp_enqueue_style('wordplate', elixir('styles/wordplate.css'));
 
-    // wp_register_script('wordplate', get_template_directory_uri().'/assets/scripts/wordplate.js', '', '', true);
+    // wp_register_script('wordplate', elixir('scripts/wordplate.js'), '', '', true);
     // wp_enqueue_script('wordplate');
 });
 
