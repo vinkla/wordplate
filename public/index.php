68435353ba8685be3bc28658ebf7ef14605ae6d4
Vincent Klaiber <vincentklaiber@gmail.com>
2016-03-05 23:32:32 +0100

    Whoops, missed one

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of WordPlate.
- *
- * (c) Vincent Klaiber <hello@vinkla.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
