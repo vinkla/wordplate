6d5ce234fdc6719f4d7f8d60253aa37f7f612a69
Vincent Klaiber <vincentklaiber@gmail.com>
2016-03-10 10:43:11 +0100

    Remove default analytics key

--- public/themes/wordplate/library/theme.php
+++ public/themes/wordplate/library/theme.php
@@ -26,7 +26,6 @@ add_action('after_setup_theme', function () {
 add_theme_support('soil-clean-up');
 add_theme_support('soil-disable-asset-versioning');
 add_theme_support('soil-disable-trackbacks');
-add_theme_support('soil-google-analytics', env('GOOGLE_ANALYTICS'), 'wp_head');
 add_theme_support('soil-js-to-footer');
 add_theme_support('soil-nice-search');
 add_theme_support('soil-relative-urls');
