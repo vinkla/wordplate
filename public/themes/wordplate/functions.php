45b70341d54807aa9eb9de480b7e659583a2259a
Vincent Klaiber <vincentklaiber@gmail.com>
2016-04-28 10:37:44 +0200

    Add title-tag and html5 support

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -19,6 +19,19 @@ add_action('after_setup_theme', function () {
     // Add support for post formats.
     //add_theme_support('post-formats', ['aside', 'audio', 'gallery', 'image', 'link', 'quote', 'video']);
 
+    // Add title tag theme support.
+    add_theme_support('title-tag');
+
+    // Add HTML5 support.
+    add_theme_support('html5', [
+        'caption',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'search-form',
+        'widgets',
+    ]);
+
     // Add primary WordPress menu.
     register_nav_menu('primary-menu', __('Primary Menu', 'wordplate'));
 });
