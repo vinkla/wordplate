71e4f3c87ba64326b159f45cbbc8aff64f1aa76d
Vincent Klaiber <vincentklaiber@gmail.com>
2017-01-04 12:44:21 +0100

    Update changelog

--- public/index.php
+++ public/index.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-/**
+/*
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  */
 
-/**
+/*
  * Tells WordPress to load the WordPress theme and output it.
  *
  * @var bool
  */
 define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template */
+/* Loads the WordPress Environment and Template */
 require __DIR__.'/wordpress/wp-blog-header.php';
