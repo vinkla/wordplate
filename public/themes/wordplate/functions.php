fa4e53812a6f006093fc33f5d2479239b56ca262
Vincent Klaiber <vincentklaiber@gmail.com>
2017-03-11 16:29:57 +0100

    Use the new template_path helper

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Register plugin helpers.
-require __DIR__.'/library/plate.php';
+require template_path('library/plate.php');
 
 // Set theme defaults.
 add_action('after_setup_theme', function () {
