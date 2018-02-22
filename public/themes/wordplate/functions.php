c666353aeec16361b47be6194a5fbd504db233de
Vincent Klaiber <vincentklaiber@gmail.com>
2018-02-22 14:39:26 +0100

    Update default structure

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Register plugin helpers.
-require template_path('library/plate.php');
+require template_path('includes/plugins/plate.php');
 
 // Set theme defaults.
 add_action('after_setup_theme', function () {
