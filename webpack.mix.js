83b787fd934e27205b64aa1b1a90790cdab268f3
Vincent Klaiber <vincentklaiber@gmail.com>
2018-08-06 15:26:54 +0200

    Update resource directory structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,7 @@ const theme = process.env.WP_THEME;
 mix.setResourceRoot('../');
 mix.setPublicPath(`public/themes/${theme}/assets`);
 
-mix.js('resources/assets/scripts/app.js', 'scripts');
-mix.sass('resources/assets/styles/app.scss', 'styles');
+mix.js('resources/scripts/app.js', 'scripts');
+mix.sass('resources/styles/app.scss', 'styles');
 
 mix.version();
