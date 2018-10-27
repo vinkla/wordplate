8be592e276d17589f592fb1d89c411f7d21e0679
Vincent Klaiber <vincentklaiber@gmail.com>
2018-10-27 17:46:41 +0200

    Added manifest.json file

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -3,7 +3,17 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
+  <meta name="application-name" content="<?php bloginfo('name'); ?>">
+  <meta name="mobile-web-app-capable" content="yes">
   <meta name="theme-color" content="#6d9aea">
+
+  <link rel="apple-touch-icon" href="<?php echo asset('assets/images/icon-192.png'); ?>">
+  <link rel="icon" sizes="192x192" href="<?php echo asset('assets/images/favicon.png'); ?>">
+  <link rel="shortcut icon" href="<?php echo asset('assets/images/favicon.png'); ?>">
+  <link rel="manifest" href="<?php echo asset('manifest.json'); ?>">
 
   <?php wp_head(); ?>
 </head>
