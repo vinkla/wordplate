9820dc2096289edbd8b9fb48417ddda7d3866182
Vincent Klaiber <vincentklaiber@gmail.com>
2019-05-22 07:50:40 +0200

    Remove enqueue scripts

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -32,16 +32,6 @@ add_action('after_setup_theme', function () {
     ]);
 });
 
-// Enqueue and register scripts the right way.
-add_action('wp_enqueue_scripts', function () {
-    wp_deregister_script('jquery');
-
-    // wp_enqueue_style('wordplate', mix('styles/app.css'));
-
-    // wp_register_script('wordplate', mix('scripts/app.js'), '', '', true);
-    // wp_enqueue_script('wordplate');
-});
-
 // Remove JPEG compression.
 add_filter('jpeg_quality', function () {
     return 100;
