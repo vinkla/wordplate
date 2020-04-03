f2e88185af0951d1b7f54a4612bcaea8423e9029
Vincent Klaiber <vincent@doubledip.se>
2020-04-03 19:05:35 +0200

    Update viewport meta tag

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -2,9 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#6d9aea">
-
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
