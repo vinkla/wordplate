113198ada8a3181ec16e89894e39dfa99879e0c9
Vincent Klaiber <vincent@doubledip.se>
2020-04-03 19:04:07 +0200

    Don't install plate by default

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -2,27 +2,16 @@
 
 declare(strict_types=1);
 
-// Register plugin helpers.
-require get_theme_file_path('includes/plugins/plate.php');
-
-// Set theme defaults.
 add_action('after_setup_theme', function () {
-    // Disable the admin toolbar.
-    show_admin_bar(false);
-
-    // Add post thumbnails support.
     add_theme_support('post-thumbnails');
 
-    // Add title tag theme support.
     add_theme_support('title-tag');
 
-    // Register navigation menus.
     register_nav_menus([
         'navigation' => __('Navigation', 'wordplate'),
     ]);
 });
 
-// Remove JPEG compression.
 add_filter('jpeg_quality', function () {
     return 100;
 }, 10, 2);
