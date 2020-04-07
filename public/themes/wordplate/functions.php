79e4ba7a7355b5f736f9fb26b855bc07d7c8d04a
Vincent Klaiber <vincent@doubledip.se>
2020-04-07 09:46:25 +0200

    Revert "Add show_admin_bar"
    This reverts commit e49bc146af87924ed09012efcd5e4038cd285d7d.

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 add_action('after_setup_theme', function () {
-    show_admin_bar(false);
-
     add_theme_support('post-thumbnails');
 
     add_theme_support('title-tag');
