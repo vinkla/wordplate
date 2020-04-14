acaf2415a7cd12c307cfcd6c847ab806480015c9
Vincent Klaiber <vincent@doubledip.se>
2020-04-14 09:10:17 +0200

    Load assets by default
    As discussed in #249

--- public/themes/wordplate/footer.php
+++ public/themes/wordplate/footer.php
@@ -1,3 +1,4 @@
+  <script src="<?php echo get_theme_file_uri('assets/scripts/app.js'); ?>"></script>
   <?php wp_footer(); ?>
 </body>
 </html>
--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -4,6 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#6d9aea">
+  <link rel="stylesheet" href="<?php echo get_theme_file_uri('assets/styles/app.css'); ?>">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
