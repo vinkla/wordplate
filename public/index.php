8b228dafa837acb2d753c172013ebc5923b0c354
Vincent Klaiber <vincent@doubledip.se>
2020-05-23 14:39:55 +0200

    Remove license headers

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of WordPlate.
- *
- * (c) Vincent Klaiber <hello@doubledip.se>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 /*
