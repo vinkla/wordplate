25cbcaecd64f6e8bd8b58736b05612fdec204964
Vincent Klaiber <vincent@doubledip.se>
2020-11-19 09:55:21 +0100

    Use short echo tag

--- public/themes/wordplate/footer.php
+++ public/themes/wordplate/footer.php
@@ -1,4 +1,4 @@
-  <script src="<?php echo get_theme_file_uri('assets/app.js'); ?>"></script>
+  <script src="<?= get_theme_file_uri('assets/app.js') ?>"></script>
   <?php wp_footer(); ?>
 </body>
 </html>
--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#6d9aea">
-  <link rel="stylesheet" href="<?php echo get_theme_file_uri('assets/app.css'); ?>">
+  <link rel="stylesheet" href="<?= get_theme_file_uri('assets/app.css') ?>">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
