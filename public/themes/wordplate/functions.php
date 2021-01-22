48b2d865d374dcabf4d57ce655a82c36a40c29f8
Vincent Klaiber <vincent@doubledip.se>
2021-01-22 14:05:42 +0100

    Remove jpeg compression

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -1,15 +1,13 @@
 <?php
 
-add_action('after_setup_theme', function () {
+function wordplate_after_setup_theme()
+{
     add_theme_support('post-thumbnails');
-
     add_theme_support('title-tag');
 
     register_nav_menus([
         'navigation' => __('Navigation'),
     ]);
-});
+}
 
-add_filter('jpeg_quality', function () {
-    return 100;
-}, 10, 2);
+add_action('after_setup_theme', 'wordplate_after_setup_theme');
