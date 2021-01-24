6c889cda739254981669d1bc3e11e8202a2929de
Vincent Klaiber <vincent@doubledip.se>
2021-01-24 13:34:08 +0100

    Update functions.php

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -1,13 +1,10 @@
 <?php
 
-function wordplate_after_setup_theme()
-{
+add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
 
     register_nav_menus([
         'navigation' => __('Navigation'),
     ]);
-}
-
-add_action('after_setup_theme', 'wordplate_after_setup_theme');
+});
