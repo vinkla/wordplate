a64b7ed856664660aeb17fbd4161b52ad305df09
Vincent Klaiber <vincent@doubledip.se>
2021-06-01 12:04:58 +0200

    Added wp_body_open

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -7,6 +7,7 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
     <header>
         <nav role="navigation">
             <?php wp_nav_menu(['theme_location' => 'navigation']); ?>
