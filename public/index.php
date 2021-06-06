d9f05c8775022dad63684ad161403ff452dd73f4
Vincent Klaiber <vincent@doubledip.se>
2021-06-06 16:17:27 +0200

    Remove comments in `index.php` and `wp-config.php``

--- public/index.php
+++ public/index.php
@@ -1,26 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Theming WordPress
-|--------------------------------------------------------------------------
-|
-| We need to define and tell WordPress to load the WordPress theme and
-| output it to the client's browser.
-|
-*/
-
 define('WP_USE_THEMES', true);
-
-/*
-|--------------------------------------------------------------------------
-| Booting WordPress
-|--------------------------------------------------------------------------
-|
-| This is the front to the WordPress application. This file doesn't do
-| anything, but loads wp-blog-header.php which does and tells WordPress to
-| load the theme.
-|
-*/
 
 require __DIR__ . '/wordpress/wp-blog-header.php';
--- public/wp-config.php
+++ public/wp-config.php
@@ -1,81 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Register The Composer Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually".
-|
-*/
-
 require __DIR__ . '/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new WordPlate application instance
-| which serves as the "glue" for all the components of WordPlate, and is
-| the container for the system binding all of the various parts.
-|
-*/
-
-$application = new WordPlate\Application(
-    realpath(__DIR__ . '/../')
-);
-
-/*
-|--------------------------------------------------------------------------
-| Custom WordPress Constants
-|--------------------------------------------------------------------------
-|
-| Below you can add custom WordPress constants which aren't specified in
-| application class. You may of course use the env() helper function.
-|
-*/
-
-// define('WP_ALLOW_MULTISITE', env('WP_ALLOW_MULTISITE', true));
-
-/*
-|--------------------------------------------------------------------------
-| WordPress Database Table Prefix
-|--------------------------------------------------------------------------
-|
-| You can have multiple installations in one database if you give each
-| a unique prefix. Only numbers, letters, and underscores please!
-|
-*/
 
 $table_prefix = env('WP_PREFIX', 'wp_');
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can start the engine. This bootstraps
-| the framework and gets it ready for use, then it will load up this
-| application so that we can run it and send the responses back to the
-| browser and delight our users.
-|
-*/
-
+$application = new WordPlate\Application(realpath(__DIR__ . '/../'));
 $application->run();
-
-/*
-|--------------------------------------------------------------------------
-| Bootstrap WordPress Framework
-|--------------------------------------------------------------------------
-|
-| The settings file is used to set up and fix common variables and include
-| the WordPress procedural and class library. We also need to keep this
-| include here in order to support WP-CLI.
-|
-*/
 
 require_once ABSPATH . 'wp-settings.php';
