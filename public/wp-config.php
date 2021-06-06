a32df286d70aa199dd65dfc9dc6b202871e9a96f
Vincent Klaiber <vincent@doubledip.se>
2021-06-06 16:01:11 +0200

    Rename database table prefix variable

--- public/wp-config.php
+++ public/wp-config.php
@@ -5,6 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $application = new WordPlate\Application(realpath(__DIR__ . '/../'));
 $application->run();
 
-$table_prefix = env('WP_PREFIX', 'wp_');
+$table_prefix = env('DB_TABLE_PREFIX', 'wp_');
 
 require_once ABSPATH . 'wp-settings.php';
