292e0e928844a93ee2cacb29a10af01cff47645a
Vincent Klaiber <vincent@doubledip.se>
2022-07-01 11:06:39 +0200

    Revert "Replace env with $_ENV"

--- public/wp-config.php
+++ public/wp-config.php
@@ -11,23 +11,23 @@ use Symfony\Component\HttpFoundation\Request;
 Dotenv::createImmutable(realpath(__DIR__ . '/../'))->safeLoad();
 
 // Set the environment type.
-define('WP_ENVIRONMENT_TYPE', $_ENV['WP_ENVIRONMENT_TYPE'] ?? 'production');
+define('WP_ENVIRONMENT_TYPE', env('WP_ENVIRONMENT_TYPE', 'production'));
 
 // For developers: WordPress debugging mode.
-$isDebugModeEnabled = $_ENV['WP_DEBUG'] ?? false;
+$isDebugModeEnabled = env('WP_DEBUG', false);
 define('WP_DEBUG', $isDebugModeEnabled);
-define('WP_DEBUG_LOG', $_ENV['WP_DEBUG_LOG'] ?? false);
-define('WP_DEBUG_DISPLAY', $_ENV['WP_DEBUG_DISPLAY'] ?? $isDebugModeEnabled);
-define('SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG'] ?? $isDebugModeEnabled);
+define('WP_DEBUG_LOG', env('WP_DEBUG_LOG', false));
+define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY', $isDebugModeEnabled));
+define('SCRIPT_DEBUG', env('SCRIPT_DEBUG', $isDebugModeEnabled));
 
 // The database configuration with database name, username, password,
 // hostname charset and database collae type.
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_HOST', $_ENV['DB_HOST'] ?? '127.0.0.1');
-define('DB_CHARSET', $_ENV['DB_CHARSET'] ?? 'utf8mb4');
-define('DB_COLLATE', $_ENV['DB_COLLATE'] ?? 'utf8mb4_unicode_ci');
+define('DB_NAME', env('DB_NAME'));
+define('DB_USER', env('DB_USER'));
+define('DB_PASSWORD', env('DB_PASSWORD'));
+define('DB_HOST', env('DB_HOST', '127.0.0.1'));
+define('DB_CHARSET', env('DB_CHARSET', 'utf8mb4'));
+define('DB_COLLATE', env('DB_COLLATE', 'utf8mb4_unicode_ci'));
 
 // Detect HTTPS behind a reverse proxy or a load balancer.
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
@@ -35,55 +35,55 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 }
 
 // Set the unique authentication keys and salts.
-define('AUTH_KEY', $_ENV['AUTH_KEY']);
-define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
-define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
-define('NONCE_KEY', $_ENV['NONCE_KEY']);
-define('AUTH_SALT', $_ENV['AUTH_SALT']);
-define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
-define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
-define('NONCE_SALT', $_ENV['NONCE_SALT']);
+define('AUTH_KEY', env('AUTH_KEY'));
+define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+define('NONCE_KEY', env('NONCE_KEY'));
+define('AUTH_SALT', env('AUTH_SALT'));
+define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+define('NONCE_SALT', env('NONCE_SALT'));
 
 // Set the home url to the current domain.
 define('WP_HOME', $_ENV['WP_HOME'] ?? Request::createFromGlobals()->getSchemeAndHttpHost());
 
 // Set the WordPress directory path.
-define('WP_SITEURL', $_ENV['WP_SITEURL'] ?? sprintf('%s/%s', WP_HOME, $_ENV['WP_DIR'] ?? 'wordpress'));
+define('WP_SITEURL', env('WP_SITEURL', sprintf('%s/%s', WP_HOME, env('WP_DIR', 'wordpress'))));
 
 // Set the WordPress content directory path.
-define('WP_CONTENT_DIR', $_ENV['WP_CONTENT_DIR'] ?? realpath(__DIR__ . '/../public'));
-define('WP_CONTENT_URL', $_ENV['WP_CONTENT_URL'] ?? WP_HOME);
+define('WP_CONTENT_DIR', env('WP_CONTENT_DIR', realpath(__DIR__ . '/../public')));
+define('WP_CONTENT_URL', env('WP_CONTENT_URL', WP_HOME));
 
 // Set the trash to less days to optimize WordPress.
-define('EMPTY_TRASH_DAYS', $_ENV['EMPTY_TRASH_DAYS'] ?? 7);
+define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS', 7));
 
 // Set the default WordPress theme.
-define('WP_DEFAULT_THEME', $_ENV['WP_DEFAULT_THEME'] ?? 'wordplate');
+define('WP_DEFAULT_THEME', env('WP_DEFAULT_THEME', 'wordplate'));
 
 // Constant to configure core updates.
-define('WP_AUTO_UPDATE_CORE', $_ENV['WP_AUTO_UPDATE_CORE'] ?? 'minor');
+define('WP_AUTO_UPDATE_CORE', env('WP_AUTO_UPDATE_CORE', 'minor'));
 
 // Specify the number of post revisions.
-define('WP_POST_REVISIONS', $_ENV['WP_POST_REVISIONS'] ?? 2);
+define('WP_POST_REVISIONS', env('WP_POST_REVISIONS', 2));
 
 // Disable technical issues emails.
 define('WP_DISABLE_FATAL_ERROR_HANDLER', $_ENV['WP_DISABLE_FATAL_ERROR_HANDLER'] ?? false);
 
 // Cleanup WordPress image edits.
-define('IMAGE_EDIT_OVERWRITE', $_ENV['IMAGE_EDIT_OVERWRITE'] ?? true);
+define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE', true));
 
 // Prevent file edititing from the dashboard.
-define('DISALLOW_FILE_EDIT', $_ENV['DISALLOW_FILE_EDIT'] ?? true);
+define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT', true));
 
 // Disable WP-Cron (wp-cron.php) for faster performance.
 define('DISABLE_WP_CRON', $_ENV['DISABLE_WP_CRON'] ?? false);
 
 // Set the absolute path to the WordPress directory.
 if (!defined('ABSPATH')) {
-    define('ABSPATH', sprintf('%s/%s/', realpath(__DIR__ . '/../public'), $_ENV['WP_DIR'] ?? 'wordpress'));
+    define('ABSPATH', sprintf('%s/%s/', realpath(__DIR__ . '/../public'), env('WP_DIR', 'wordpress')));
 }
 
 // Set the database table prefix.
-$table_prefix = $_ENV['DB_TABLE_PREFIX'] ?? 'wp_';
+$table_prefix = env('DB_TABLE_PREFIX', 'wp_');
 
 require_once ABSPATH . 'wp-settings.php';
---
+++ src/helpers.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('env')) {
+    function env(string $key, mixed $default = null): mixed
+    {
+        $value = $_ENV[$key] ?? false;
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return null;
+        }
+
+        if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+            return $matches[2];
+        }
+
+        return $value;
+    }
+}
