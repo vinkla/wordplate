65b14c54c29faa185e1adf0bf889368372b7e35c
Vincent Klaiber <vincent@doubledip.se>
2022-07-02 21:09:39 +0200

    Move clean-ui into functions.php

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// Register theme defaults.
 add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
@@ -7,4 +8,60 @@ add_action('after_setup_theme', function () {
     register_nav_menus([
         'navigation' => __('Navigation'),
     ]);
+});
+
+// Remove administrator menu items.
+add_action('admin_init', function () {
+    remove_menu_page('edit-comments.php'); // Comments
+    // remove_menu_page('edit.php?post_type=page'); // Pages
+    remove_menu_page('edit.php'); // Posts
+    remove_menu_page('index.php'); // Dashboard
+    // remove_menu_page('upload.php'); // Media
+});
+
+// Remove administrator toolbar menu items.
+add_action('admin_bar_menu', function ($menu) {
+    $menu->remove_node('comments'); // Comments
+    $menu->remove_node('customize'); // Customize
+    $menu->remove_node('dashboard'); // Dashboard
+    $menu->remove_node('edit'); // Edit
+    $menu->remove_node('menus'); // Menus
+    $menu->remove_node('new-content'); // New Content
+    $menu->remove_node('search'); // Search
+    // $menu->remove_node('site-name'); // Site Name
+    $menu->remove_node('themes'); // Themes
+    $menu->remove_node('updates'); // Updates
+    $menu->remove_node('view-site'); // Visit Site
+    $menu->remove_node('view'); // View
+    $menu->remove_node('widgets'); // Widgets
+    $menu->remove_node('wp-logo'); // WordPress Logo
+}, 999);
+
+// Remove administrator dashboard widgets.
+add_action('wp_dashboard_setup', function () {
+    global $wp_meta_boxes;
+
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']); // Activity
+    // unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']); // At a Glance
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_site_health']); // Site Health Status
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']); // WordPress Events and News
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']); // Quick Draft
+});
+
+// Add custom login form logo.
+add_action('login_head', function () {
+    $url = get_theme_file_uri('favicon.svg');
+    $width = 200;
+
+    $styles = [
+        sprintf('background-image: url(%s);', $url),
+        sprintf('width: %dpx;', $width),
+        'background-position: center;',
+        'background-size: contain;',
+    ];
+
+    echo sprintf(
+        '<style> .login h1 a { %s } </style>',
+        implode('', $styles)
+    );
 });
