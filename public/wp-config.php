8d88e727fc1f670044e9df9bd90cf2043f5399d4
Vincent Klaiber <vincent@doubledip.se>
2022-08-01 14:26:26 +0200

    Reorder wordpress constants

--- public/wp-config.php
+++ public/wp-config.php
@@ -13,6 +13,9 @@ Dotenv::createImmutable(realpath(__DIR__ . '/../'))->safeLoad();
 // Set the environment type.
 define('WP_ENVIRONMENT_TYPE', env('WP_ENVIRONMENT_TYPE', 'production'));
 
+// Set the default WordPress theme.
+define('WP_DEFAULT_THEME', env('WP_DEFAULT_THEME', 'wordplate'));
+
 // For developers: WordPress debugging mode.
 $isDebugModeEnabled = env('WP_DEBUG', false);
 define('WP_DEBUG', $isDebugModeEnabled);
@@ -29,11 +32,6 @@ define('DB_HOST', env('DB_HOST', '127.0.0.1'));
 define('DB_CHARSET', env('DB_CHARSET', 'utf8mb4'));
 define('DB_COLLATE', env('DB_COLLATE', 'utf8mb4_unicode_ci'));
 
-// Detect HTTPS behind a reverse proxy or a load balancer.
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
-
 // Set the unique authentication keys and salts.
 define('AUTH_KEY', env('AUTH_KEY'));
 define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
@@ -43,6 +41,11 @@ define('AUTH_SALT', env('AUTH_SALT'));
 define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
 define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
 define('NONCE_SALT', env('NONCE_SALT'));
+
+// Detect HTTPS behind a reverse proxy or a load balancer.
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
 
 // Set the home url to the current domain.
 define('WP_HOME', env('WP_HOME', Request::createFromGlobals()->getSchemeAndHttpHost()));
@@ -54,23 +57,14 @@ define('WP_SITEURL', env('WP_SITEURL', sprintf('%s/%s', WP_HOME, env('WP_DIR', '
 define('WP_CONTENT_DIR', env('WP_CONTENT_DIR', realpath(__DIR__ . '/../public')));
 define('WP_CONTENT_URL', env('WP_CONTENT_URL', WP_HOME));
 
-// Set the trash to less days to optimize WordPress.
-define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS', 7));
-
-// Set the default WordPress theme.
-define('WP_DEFAULT_THEME', env('WP_DEFAULT_THEME', 'wordplate'));
-
 // Disable WordPress auto updates.
 define('AUTOMATIC_UPDATER_DISABLED', env('AUTOMATIC_UPDATER_DISABLED', true));
 
-// Limit the number of post revisions.
-define('WP_POST_REVISIONS', env('WP_POST_REVISIONS', true));
+// Set the trash to less days to optimize WordPress.
+define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS', 7));
 
-// Disable technical issues emails.
-define('WP_DISABLE_FATAL_ERROR_HANDLER', env('WP_DISABLE_FATAL_ERROR_HANDLER', false));
-
-// Cleanup WordPress image edits.
-define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE', true));
+// Disable WP-Cron (wp-cron.php) for faster performance.
+define('DISABLE_WP_CRON', env('DISABLE_WP_CRON', false));
 
 // Prevent file edititing from the dashboard.
 define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT', true));
@@ -78,8 +72,14 @@ define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT', true));
 // Disable plugin and theme updates and installation from the dashboard.
 define('DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS', true));
 
-// Disable WP-Cron (wp-cron.php) for faster performance.
-define('DISABLE_WP_CRON', env('DISABLE_WP_CRON', false));
+// Cleanup WordPress image edits.
+define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE', true));
+
+// Disable technical issues emails.
+define('WP_DISABLE_FATAL_ERROR_HANDLER', env('WP_DISABLE_FATAL_ERROR_HANDLER', false));
+
+// Limit the number of post revisions.
+define('WP_POST_REVISIONS', env('WP_POST_REVISIONS', 2));
 
 // Set the absolute path to the WordPress directory.
 if (!defined('ABSPATH')) {
