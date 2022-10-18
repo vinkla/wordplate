687cf83f1ff53102e2aae1853ea6546c0824fa86
Vincent Klaiber <vincent@doubledip.se>
2022-10-18 14:26:22 +0200

    Fix syntax errors

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -22,7 +22,7 @@ add_action('wp_enqueue_scripts', function () {
     ) {
         wp_enqueue_script('vite', 'http://localhost:5173/@vite/client');
         wp_enqueue_script('wordplate', 'http://localhost:5173/resources/js/index.js');
-    } else if (file_exists($manifestPath)) {
+    } elseif (file_exists($manifestPath)) {
         $manifest = json_decode(file_get_contents($manifestPath), true);
         wp_enqueue_script('wordplate', get_theme_file_uri('assets/' . $manifest['resources/js/index.js']['file']));
         wp_enqueue_style('wordplate', get_theme_file_uri('assets/' . $manifest['resources/js/index.css']['file']));
