d6f03fc02751b8116d86960c34c1a6a96e2f2fc5
Vincent Klaiber <vinkla@users.noreply.github.com>
2024-05-03 10:55:07 +0200

    Update public/themes/wordplate/functions.php

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -105,4 +105,4 @@ add_filter('wp_mail_from', fn() => env('MAIL_FROM_ADDRESS', 'hello@example.com')
 add_filter('wp_mail_from_name', fn() => env('MAIL_FROM_NAME', 'Example'));
 
 // Rename wp-json to api.
-add_filter('rest_url_prefix', fn () => 'api');
+add_filter('rest_url_prefix', fn() => 'api');
